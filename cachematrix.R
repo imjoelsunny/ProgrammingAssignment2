6d391ac7d3f42e3972285f78368d810a93559178
Joel <joelsunny2011@gmail.com>
2019-04-03 19:57:01 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
+  set <- function(y) 
+    {                    ## define the set function to assign new 
+      x <<- y                             ## value of matrix in parent environment
+      inv <<- NULL                        ## if there is a new matrix, reset inv to NULL
+  }
+  get <- function() x                     ## define the get fucntion - returns value of the matrix argument
+  
+  setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
+  getinverse <- function() inv                     ## gets the value of inv where called
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
 
-}
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {                     #if inverse matrix is not NULL
+      message("getting cached data")      #type message: Getting Cached Invertible Matrix
+      return(inv)                         #return the invertible matrix
+    }
+    
+    #if value of the invertible matrix is NULL then
+    data <- x$get()                          #get the original Matrix Data
+    inv <- solve(data, ...)                  #use solve function to inverse the matrix
+    x$setinverse(inv)                        #set the invertible matrix
+    return(inv)                              #return the invertible matrix
+  }
+  
+  
